55a5aa4e72fd6d217ebb5da342572e67b9fc44d0
himanshusingh2308 <himanshu@flobiz.in>
2021-06-09 17:09:11 +0530

    added release notes

--- api/controllers/VersionController.js
+++ api/controllers/VersionController.js
@@ -511,26 +511,27 @@ module.exports = {
 
           const sha512 = asset.hash ? asset.hash : null;
 
-          var latestYml =
-            "version: " +
-            latestVersion.name +
-            "\nfiles:" +
-            "\n  - url: " +
-            downloadPath +
-            "\n    sha512: " +
-            sha512 +
-            "\n    size: " +
-            asset.size +
-            "\nreleaseDate: " +
-            latestVersion.updatedAt +
-            "\npath: " +
-            downloadPath +
-            "\nsha512: " +
-            sha512 +
-            "\nsize: " +
-            asset.size +
-            "\nhardUpdate: " +
-            true;
+          var latestYml = `version: ${latestVersion.name} \nfiles:\n  - url: ${downloadPath}\n    sha512: ${sha512}\n    size: ${asset.size}
+          \nreleaseDate: ${latestVersion.updatedAt}\npath: ${downloadPath}\nsha512: ${sha512}\nsize: ${asset.size}\nhardUpdate: true\nnotes: ${latestVersion.notes}`;
+          // "version: " +
+          // latestVersion.name +
+          // "\nfiles:" +
+          // "\n  - url: " +
+          // downloadPath +
+          // "\n    sha512: " +
+          // sha512 +
+          // "\n    size: " +
+          // asset.size +
+          // "\nreleaseDate: " +
+          // latestVersion.updatedAt +
+          // "\npath: " +
+          // downloadPath +
+          // "\nsha512: " +
+          // sha512 +
+          // "\nsize: " +
+          // asset.size +
+          // "\nhardUpdate: " +
+          // true;
 
           res.ok(latestYml);
         } else {
